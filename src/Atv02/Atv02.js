f4404caae1e6ad99de591d866b08700feb931769
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2023-11-12 23:43:05 -0300

    Update Atv02.js

--- src/Atv02/Atv02.js
+++ src/Atv02/Atv02.js
@@ -4,7 +4,7 @@ import Contador from "./Contador";
 function Atv02() {
     return (
         <>
-            <h1>Atividade 01</h1>
+            <h1>Atividade 02</h1>
             <Contador />
             <Link to="/">Retornar para a página inicial</Link>
         </>
