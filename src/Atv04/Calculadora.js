f63a5808f4c1a370a92b90d7409e795c7fcbfcc9
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2023-11-20 15:11:21 -0300

    Add files via upload

--- src/Atv04/Calculadora.js
+++ src/Atv04/Calculadora.js
@@ -47,33 +47,33 @@ export default function Calculadora() {
                     <input type="text" id="display" className="bg-dark text-white input_calculadora" value={Display}/>
                 </div>
                 <div id="linha2" className="row mx-auto mt-3 py-1">
-                    <button id="AC" onClick={() => limpaDisplay()} className="btn btn-light rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>AC</button>
-                    <button id="+/-" onClick={() => mudaSinal()} className="btn btn-light rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>+/-</button>
-                    <button id="%" onClick={() => porcentagem()} className="btn btn-light rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>%</button>
-                    <button id="/" onClick={() => addDisplay('/')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>/</button>
+                    <button id="AC" onClick={() => limpaDisplay()} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">AC</button>
+                    <button id="+/-" onClick={() => mudaSinal()} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">+/-</button>
+                    <button id="%" onClick={() => porcentagem()} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">%</button>
+                    <button id="/" onClick={() => addDisplay('/')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg botao_calculadora">/</button>
                 </div>
                 <div id="linha3" className="row mx-auto mt-3 py-1">
-                    <button id="7" onClick={() => addDisplay('7')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>7</button>
-                    <button id="8" onClick={() => addDisplay('8')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>8</button>
-                    <button id="9" onClick={() => addDisplay('9')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>9</button>
-                    <button id="x" onClick={() => addDisplay('*')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>x</button>
+                    <button id="7" onClick={() => addDisplay('7')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">7</button>
+                    <button id="8" onClick={() => addDisplay('8')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">8</button>
+                    <button id="9" onClick={() => addDisplay('9')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">9</button>
+                    <button id="x" onClick={() => addDisplay('*')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg botao_calculadora">x</button>
                 </div>
                 <div id="linha4" className="row mx-auto mt-3 py-1">
-                    <button id="4" onClick={() => addDisplay('4')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>4</button>
-                    <button id="5" onClick={() => addDisplay('5')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>5</button>
-                    <button id="6" onClick={() => addDisplay('6')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>6</button>
-                    <button id="-" onClick={() => addDisplay('-')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>-</button>
+                    <button id="4" onClick={() => addDisplay('4')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">4</button>
+                    <button id="5" onClick={() => addDisplay('5')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">5</button>
+                    <button id="6" onClick={() => addDisplay('6')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">6</button>
+                    <button id="-" onClick={() => addDisplay('-')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg botao_calculadora">-</button>
                 </div>
                 <div id="linha5" className="row mx-auto mt-3 py-1">
-                    <button id="1" onClick={() => addDisplay('1')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>1</button>
-                    <button id="2" onClick={() => addDisplay('2')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>2</button>
-                    <button id="3" onClick={() => addDisplay('3')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>3</button>
-                    <button id="+" onClick={() => addDisplay('+')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>+</button>
+                    <button id="1" onClick={() => addDisplay('1')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">1</button>
+                    <button id="2" onClick={() => addDisplay('2')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">2</button>
+                    <button id="3" onClick={() => addDisplay('3')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">3</button>
+                    <button id="+" onClick={() => addDisplay('+')} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg botao_calculadora">+</button>
                 </div>
                 <div id="linha6" className="row mx-auto mt-3 py-1">
                     <button id="0" onClick={() => addDisplay('0')} className="btn btn-secondary rounded-pill py-4 ms-5 btn-lg" style={{ width: 250 + "px"}}>0</button>
-                    <button id="." onClick={() => addDisplay('.')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>.</button>
-                    <button id="=" onClick={() => calcular()} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg" style={{ width: 100 + "px"}}>=</button>
+                    <button id="." onClick={() => addDisplay('.')} className="btn btn-secondary rounded-circle py-4 ms-5 btn-lg botao_calculadora">.</button>
+                    <button id="=" onClick={() => calcular()} className="btn btn-warning rounded-circle py-4 ms-5 btn-lg botao_calculadora">=</button>
                 </div>
             </div>
         </div>
