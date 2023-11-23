5305059bc3eb92bc5dcd29a05a04575b435db5fb
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2023-11-23 17:17:57 -0300

    Add files via upload

--- src/Atv05/Memoria.js
+++ src/Atv05/Memoria.js
@@ -1,3 +1,4 @@
+import './Memoria.css';
 import { useState, useEffect } from "react";
 import embaralharImagem from "./embaralharImagem";
 
@@ -30,7 +31,7 @@ export default function Memoria() {
                     document.getElementById(ArrayPecaSelecionada[2]).style.display = "none";
                 }
 
-            }, 100);
+            }, 500);
 
             setQtdSelecionado(0);
             setArrayPecaSelecionada([]);
@@ -49,29 +50,29 @@ export default function Memoria() {
         <div id="principal" className="container-fluid">
             <div id="caixa" className="mt-2 bg-dark mx-auto p-4" style={{ width: 670 + "px"}}>
                 <div id="linha1" className="row">
-                    <h1>Jogo da memória</h1>
+                    <h1 className="text-light">Jogo da memória</h1>
                 </div>
                 <div id="linha2" className="row mx-auto mt-3 py-1">
-                    <button onClick={() => {virarPeca(0)}} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">
+                    <button onClick={() => {virarPeca(0)}} className="botao_memoria">
                         <img id="0" src={ArrayImagem[0]} style={{ display: "none"}} />
                     </button>
-                    <button onClick={() => {virarPeca(1)}} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">
+                    <button onClick={() => {virarPeca(1)}} className="botao_memoria">
                         <img id="1" src={ArrayImagem[1]} style={{ display: "none"}}/>
                     </button>
                 </div>
                 <div id="linha3" className="row mx-auto mt-3 py-1">
-                    <button onClick={() => {virarPeca(2)}} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">
+                    <button onClick={() => {virarPeca(2)}} className="botao_memoria">
                         <img id="2" src={ArrayImagem[2]} style={{ display: "none"}}/>
                     </button>
-                    <button onClick={() => {virarPeca(3)}} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">
+                    <button onClick={() => {virarPeca(3)}} className="botao_memoria">
                         <img id="3" src={ArrayImagem[3]} style={{ display: "none"}}/>
                     </button>
                 </div>
                 <div id="linha4" className="row mx-auto mt-3 py-1">
-                    <button onClick={() => {virarPeca(4)}} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">
+                    <button onClick={() => {virarPeca(4)}} className="botao_memoria">
                         <img id="4" src={ArrayImagem[4]} style={{ display: "none"}}/>
                     </button>
-                    <button onClick={() => {virarPeca(5)}} className="btn btn-light rounded-circle py-4 ms-5 btn-lg botao_calculadora">
+                    <button onClick={() => {virarPeca(5)}} className="botao_memoria">
                         <img id="5" src={ArrayImagem[5]} style={{ display: "none"}}/>
                     </button>
                 </div>
